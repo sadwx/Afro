5ab16f35171ade7758bb8b11bab198220181344b
Zoe <sfenc101@gmail.com>
2020-03-29 18:34:16 +0800

    add punk player

---
+++ Afro Aggro/Assets/Scripts/PunkAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunkAttack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("A");
+            attack();
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Debug.Log("J");
+            attack();
+        }
+    }
+
+    void attack(){
+        // gameObject.GetComponentInChildren()
+    }
+}
